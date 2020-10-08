3dc40ce5795df8eb604a4bded727a3bee249509e
angubenko <angubenko@gmail.com>
2020-10-07 22:21:06 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+import telegram
+
+from dotenv import load_dotenv
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+
+def send_message(message):
+    return bot.send_message(chat_id=CHAT_ID, text=message)
+
+
+def main():
+    while True:
+        try:
+            send_message("Проверка связи")
+            time.sleep(600)  # опрашивать раз в пять минут
+
+        except Exception as e:
+            print(f'Бот упал с ошибкой: {e}')
+            time.sleep(5)
+            continue
+
+
+if __name__ == '__main__':
+    main()
